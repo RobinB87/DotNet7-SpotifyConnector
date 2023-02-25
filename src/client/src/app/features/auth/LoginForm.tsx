5ee7f32cc80355f57cc98b46412e145fe8ee084e
Robin Bette <robinbette@hotmail.com>
2023-02-25 16:18:16 +0100

    prettier

--- src/client/src/app/features/auth/LoginForm.tsx
+++ src/client/src/app/features/auth/LoginForm.tsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from "react";
 
-import agent from "../../api/agent"
+import agent from "../../api/agent";
 
 export const LoginForm = () => {
-    const [uri, setUri] = useState("");
+  const [uri, setUri] = useState("");
 
-    const handleClick = async () => {
-        setUri(await agent.Auth.login());
-    }
+  const handleClick = async () => {
+    setUri(await agent.Auth.login());
+  };
 
-    useEffect(() => {
-        if (uri) window.open(uri, "_blank");
-    }, [uri])
+  useEffect(() => {
+    if (uri) window.open(uri, "_blank");
+  }, [uri]);
 
-    return (
-        <>
-            <button onClick={() => {handleClick()}}>Login</button>
-        </>
-    )
-}
+  return (
+    <>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
+};
 
 export default LoginForm;
