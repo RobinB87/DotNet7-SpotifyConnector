0a26a54740176cd50163dcc418c9a488f07102e2
Robin Bette <robinbette@hotmail.com>
2023-03-01 11:43:48 +0100

    add basic form

--- src/client/src/app/features/auth/SpotifyLogin.tsx
+++ src/client/src/app/features/auth/SpotifyLogin.tsx
@@ -12,24 +12,22 @@ const SpotifyLogin = () => {
   return (
     <>
       {data && (
-        <>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <Box sx={{ width: "50%", textAlign: "center", m: 20 }}>
-              <Card variant="outlined">
-                <CardContent>
-                  <Typography sx={{ fontSize: 18 }} color="text.secondary">
-                    Please login to Spotify
-                  </Typography>
-                </CardContent>
-                <CardActions style={{ justifyContent: "center" }}>
-                  <Button href={data} variant="contained" color="primary">
-                    Login with Spotify
-                  </Button>
-                </CardActions>
-              </Card>
-            </Box>
-          </div>
-        </>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Box sx={{ width: "50%", textAlign: "center", m: 20 }}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography sx={{ fontSize: 18 }} color="text.secondary">
+                  Please login to Spotify
+                </Typography>
+              </CardContent>
+              <CardActions style={{ justifyContent: "center" }}>
+                <Button href={data} variant="contained" color="primary">
+                  Login with Spotify
+                </Button>
+              </CardActions>
+            </Card>
+          </Box>
+        </div>
       )}
     </>
   );
--- src/client/src/app/features/tracks/TracksAdd.tsx
+++ src/client/src/app/features/tracks/TracksAdd.tsx
@@ -1,5 +1,23 @@
+import { Box, Button, Card, CardActions, CardContent, Input, TextField, Typography } from "@mui/material";
+
 const TracksAdd = () => {
-  return <div>Page under construction</div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ width: "75%", m: 2 }}>
+        <Card variant="outlined">
+          <CardContent>
+            <div style={{ marginBottom: "8px" }}>
+              <Input placeholder="Playlist id" sx={{ width: "100%" }} disableUnderline={true} />
+            </div>
+            <TextField label="Uri list" variant="outlined" multiline rows={8} sx={{ width: "100%" }} />
+          </CardContent>
+          <CardActions style={{ justifyContent: "flex-end" }}>
+            <Button variant="contained">Save</Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </div>
+  );
 };
 
 export default TracksAdd;
