57b9b2a12ef16a29ceb58a30315ad1e3e96874c2
RobinB87 <robinbette@hotmail.com>
2023-04-03 19:42:28 +0200

    setup integration test project with basic assertion

---
+++ src/tests/Tests.Integration/Controllers/AuthControllerTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests.Integration.Controllers
+{
+    public class AuthControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public AuthControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetSpotifyLoginUri_Returns_SuccessStatusCode()
+        {
+            var response = await _client.GetAsync("/auth/login");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
